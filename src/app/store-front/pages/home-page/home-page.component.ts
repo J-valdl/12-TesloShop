165ec8c57c592f27d33d3970868fac144a8c9e9b
Jonathan <contactoanonimo.x@gmail.com>
2025-05-12 17:43:32 -0600

    Configura Api

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
-//import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
+// import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [
-    ProductCardComponent
-  ],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  productsService = inject(ProductsService);
 
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({});
+    },
+  });
 }
