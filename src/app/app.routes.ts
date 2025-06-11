ac05ef549934561197cea6ddb9ca1bc5856e84cb
Jonathan <contactoanonimo.x@gmail.com>
2025-06-11 17:25:11 -0600

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [
+      // () => {
+      //   console.log('hola Mundo');
+      //   return true;
+      // },
+      //NotAuthenticatedGuard,
+    ],
+  },
+  {
     path: '',
-    loadChildren: () => import('./store-front/store-front.routes').then((m) => m.storeFrontRoutes) 
+    loadChildren: () => import('./store-front/store-front.routes').then((m) => m.storeFrontRoutes)
   }
 ];
